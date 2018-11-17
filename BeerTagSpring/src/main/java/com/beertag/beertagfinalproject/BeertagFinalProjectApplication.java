0da5b897f0f0ee584234ca55e890de9683b8bee5
Vinchenzo <vinchenco10@abv.bg>
2018-11-17 13:44:02 +0200

    Initial spring project.Initial dependencies and hibernate.cfg.xml file.Initial server port

---
+++ BeerTagSpring/src/main/java/com/beertag/beertagfinalproject/BeertagFinalProjectApplication.java
@@ -0,0 +1,12 @@
+package com.beertag.beertagfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeertagFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeertagFinalProjectApplication.class, args);
+    }
+}
